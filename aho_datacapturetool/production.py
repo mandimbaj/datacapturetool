77a86088b6d5171810f89003768d0b318d2d14c0
MANDIMBA Jadice <38686632+mandimbaj@users.noreply.github.com>
2025-12-09 18:58:53 +0100

    Change production domain in ALLOWED_HOSTS
    Updated production domain name in ALLOWED_HOSTS.

--- aho_datacapturetool/production.py
+++ aho_datacapturetool/production.py
@@ -2,7 +2,7 @@ from .settings import *
 DEBUG = True # Swich off debug for security reasons
 # Configure production domain names
 ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.azurewebsites.net',
-    'af-aho-datacapturetool-stage.azurewebsites.net',
+    'af-aho-dct-f8hnfwbcb4e6c0bg.westeurope-01.azurewebsites.net',
         'dct.aho.afro.who.int'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
 # WhiteNoise configuration
